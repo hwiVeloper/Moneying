c4da868f1a48ce4daac9eba06705bed3a8d2f24b
hwiVeloper <zziller03@gmail.com>
2017-07-04 09:59:26 +0900

    js파일 간 의존성

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,10 +15,10 @@ mix
 .copy('node_modules/bootstrap-v4-dev/dist/css/bootstrap.min.css', 'resources/assets/css/bootstrap.min.css')
 .copy('node_modules/font-awesome/css/font-awesome.min.css', 'resources/assets/css/font-awesome.min.css')
 .copy('node_modules/font-awesome/fonts/**', 'public/fonts')
-.js([
-    'resources/assets/js/app.js',
-    'node_modules/bootstrap-v4-dev/dist/js/bootstrap.min.js'
-], 'public/js')
+.js('resources/assets/js/app.js', 'public/js/app.js')
+.js('node_modules/tether/dist/js/tether.min.js'             , 'public/js/tether.js')
+.js('node_modules/jquery/dist/jquery.min.js'                , 'public/js/jquery.js')
+.js('node_modules/bootstrap-v4-dev/dist/js/bootstrap.min.js', 'public/js/bootstrap.js')
 .sass('resources/assets/sass/app.scss', 'public/scss/app.css')
 .styles([
     'resources/assets/css/bootstrap.min.css',
